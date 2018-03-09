3500291143a5892c92c1b94c7fecdf52c494ed8f
wangchenghku <wangchenghku@gmail.com>
2018-03-09 19:08:36 +0800

    Add files via upload

---
+++ tutorial3.sh
@@ -0,0 +1,71 @@
+if grep -q 'count(String message) throws RemoteException' './WordCount.java'; then
+    :
+else
+    echo "WordCount"
+fi
+
+if grep -q 'import java.rmi.\*;' './WordCounter.java'; then
+    :
+else
+    echo "WordCounter: Import RMI packages"
+fi
+if grep -q 'import java.rmi.server.\*;' './WordCounter.java'; then
+    :
+else
+    echo "WordCounter: Import RMI packages"
+fi
+
+if grep -q 'class WordCounter extends UnicastRemoteObject implements WordCount' './WordCounter.java'; then
+    :
+else
+    echo "WordCounter: Extends UnicastRemoteObject, Implements WordCount interface"
+fi
+
+if grep -q 'WordCounter() throws RemoteException' './WordCounter.java'; then
+    :
+else
+    echo "WordCounter: constructor WordCounter() and count() should throw RemoteException"
+fi
+if grep -q 'count(String message) throws RemoteException' './WordCounter.java'; then
+    :
+else
+    echo "WordCounter: constructor WordCounter() and count() should throw RemoteException"
+fi
+
+if grep -q 'System.setSecurityManager(new SecurityManager());' './WordCounter.java'; then
+    :
+else
+    echo "WordCounter: Register the service in main() "
+fi
+if grep -q 'Naming.rebind("WordCounter",[[:blank:]]*app);' './WordCounter.java'; then # ignore space
+    :
+else
+    echo "WordCounter: Register the service in main() "
+fi
+
+if grep -q 'import java.rmi.registry.\*;' './MessageBox.java'; then
+    :
+else
+    echo "MessageBox"
+fi
+
+if grep -q 'args\[0]' './MessageBox.java'; then
+    :
+else
+    echo "MessageBox"
+fi
+if grep -q 'public MessageBox(String host)[[:blank:]]*{' './MessageBox.java'; then
+    :
+else
+    echo "MessageBox"
+fi
+if grep -q 'Registry registry = LocateRegistry.getRegistry(host);' './MessageBox.java'; then
+    :
+else
+    echo "MessageBox"
+fi
+if grep -q 'wordCounter[[:blank:]]*= (WordCount)registry.lookup("WordCounter");' './MessageBox.java'; then
+    :
+else
+    echo "MessageBox"
+fi
